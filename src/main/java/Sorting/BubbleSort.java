a214811c024b4d04c91390a36f7a79e678a5c2f8
sidfarheen <farisid200@gmail.com>
2025-01-25 11:18:50 +0000

    Better variable naming and comments

--- src/main/java/Sorting/BubbleSort.java
+++ src/main/java/Sorting/BubbleSort.java
@@ -1,29 +1,35 @@
 package Sorting;
 
 public class BubbleSort {
+    public
     int[] bubbleSort(int[] nums) {
         int n = nums.length;
-        // Traverse through the array
-        for (int i = n - 1; i >= 0; i--) {
-            // Track if swaps are made
-            boolean didSwap = false;
 
-            for (int j = 0; j <= i - 1; j++) {
-                // Swap if next element is smaller
-                if (nums[j] > nums[j + 1]) {
+        //pass from 0 to n-1
+        // because last element will not have anything to be compared
+        for(int pass=0; pass< n-1; pass++){
+
+            boolean swapped = false;
+
+            //number of elements to be compared is reduced every time
+            for(int j = 0; j < n-1 - pass; j++ ){
+                if( nums[j]> nums[j+1]){
                     int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                    didSwap = true;
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                    swapped = true;
                 }
             }
-            /* Break out of loop
-            if no swaps done*/
-            if (!didSwap) {
+
+
+            // i fno comparisons happened then break
+            if(!swapped){
                 break;
             }
         }
 
+
         return nums;
+
     }
 }
--- src/main/java/Sorting/SelectionSort.java
+++ src/main/java/Sorting/SelectionSort.java
@@ -1,29 +1,32 @@
 package Sorting;
 
 public class SelectionSort {
-    public
-    int[] selectionSort(int[] nums) {
+    public  int[] selectionSort(int[] nums) {
         int n = nums.length;
 
-        //Iterate to find the min element
-        // (n-1) because last element is not considered as 1st ever
-        for (int i = 0; i < n - 1; i++) {
-            int minIndex = i;
+        // loop till second last element
+        //because there won't be anything to process when we are on the last element
+        for(int sortedIndex= 0; sortedIndex< n-1 ; sortedIndex++){
 
-            // to n because all the elements has to be compared
-            for (int j = i + 1; j < n; j++) {
-                // compare
-                if (nums[j] < nums[minIndex]) {
-                    minIndex = j;
+            int minIndex = sortedIndex;
+
+            // go till n because all the elements will be compared
+            for(int i = sortedIndex+1 ; i < n ; i++){
+                if(nums[i] < nums[minIndex]){
+                    minIndex = i ;
                 }
+
             }
 
-            //Swap the min element with 1st element
-            if (minIndex != i) {
-                int temp = nums[minIndex];
-                nums[minIndex] = nums[i];
-                nums[i] = temp;
+            //if the very first element is the minimum, do not swap
+            if( minIndex != sortedIndex){
+                int temp = nums[sortedIndex];
+                nums[sortedIndex] = nums[minIndex];
+                nums[minIndex] = temp;
             }
+
+
+
         }
         return nums;
     }
